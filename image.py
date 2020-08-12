a965f59625229e83e7f52d025ce73b7f187e1243
Karthik <karthik_vullam@cargill.com>
2020-08-12 16:03:53 +0530

    resize image added

--- image.py
+++ image.py
@@ -4,3 +4,11 @@ image = cv2.imread('jurassic-park-tour-jeep.jpg')
 print(image.shape)
 cv2.imshow("original", image)
 cv2.waitKey(0)
+
+#resizing image to 100 pixels
+r = 100.0/ image.shape[1]
+dim = (100, int(image.shape[0] * r))
+
+resized = cv2.resize(image, dim, interpolation= cv2.INTER_AREA)
+cv2.imshow("resized", resized)
+cv2.waitKey(0)
