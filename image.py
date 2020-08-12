6cb34b55675474570f95109766a01f6315e6df4e
Karthik <karthik_vullam@cargill.com>
2020-08-12 16:07:16 +0530

    rotation added

--- image.py
+++ image.py
@@ -12,3 +12,12 @@ dim = (100, int(image.shape[0] * r))
 resized = cv2.resize(image, dim, interpolation= cv2.INTER_AREA)
 cv2.imshow("resized", resized)
 cv2.waitKey(0)
+
+#calculate center and rotate the image 
+(h, w) = image.shape[:2]
+center = (w/2 , h/2)
+
+m = cv2.getRotationMatrix2D(center, 180, 1.0)
+rotated = cv2.warpAffine(image, m, (w,h))
+cv2.imshow("rotated", rotated)
+cv2.waitKey(0)
